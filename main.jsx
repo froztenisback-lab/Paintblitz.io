ae78de4304fa6a312a45168f86706063493ff3f6
froztenisback-lab <froztenisback@gmail.com>
2026-03-28 13:56:31 +0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 ﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
-import App from './game'
+import App from './game.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
