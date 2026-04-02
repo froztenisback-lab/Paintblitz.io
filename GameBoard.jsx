0fb1385531ef234c9e84d6de3bb9cb81b96675da
froztenisback-lab <froztenisback@gmail.com>
2026-04-02 23:11:59 +0700

    Update GameBoard.jsx

--- GameBoard.jsx
+++ GameBoard.jsx
@@ -1,14 +1,30 @@
-import { socket } from './main';
+// gameboard.jsx
+import { useChannel } from 'ably/react';
 
-const handleAction = (actionData) => {
-  if (mode === 'online') {
-    // Tell the server what happened
-    socket.emit('game_action', actionData);
-  } else {
-    // Run local Bot logic instead
-    runBotLogic(actionData);
-  }
-  
-  // Update your local screen immediately
-  applyActionToLocalState(actionData);
-};
+const GameBoard = () => {
+  // This replaces io.on('connection') and socket.on('paint_action')
+  const { channel } = useChannel('paintblitz-global', (message) => {
+    // This part runs when ANOTHER player sends an action
+    if (message.name === 'paint_action') {
+      applyActionToLocalState(message.data); 
+    }
+    if (message.name === 'build_action') {
+      applyActionToLocalState(message.data);
+    }
+  });
+
+  const handleAction = (actionData) => {
+    if (mode === 'online') {
+      // This replaces socket.emit('paint_action', data)
+      // We publish to the channel so EVERYONE else sees it
+      channel.publish('paint_action', actionData);
+    } else {
+      runBotLogic(actionData);
+    }
+    
+    // Update your local screen immediately
+    applyActionToLocalState(actionData);
+  };
+
+  // ... rest of your component
+}
