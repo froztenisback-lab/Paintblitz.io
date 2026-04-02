5e5f1d939b9aa55e58f843722237dac8a9e55d64
froztenisback-lab <froztenisback@gmail.com>
2026-04-02 22:58:05 +0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,17 +1,21 @@
-﻿﻿import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 import App from './game'
-import { io } from "socket.io-client";
+import * as Ably from 'ably' // Import the full Ably SDK
+import { AblyProvider, ChannelProvider } from 'ably/react'
 
-export const client = new Ably.Realtime({ 
-  authUrl: '/api/auth' 
-});
-socket.on("connect", () => {
-  console.log("Connected to server with ID:", socket.id);
-});
+// 1. Initialize the Ably Client pointing to your Vercel API route
+const client = new Ably.Realtime({ authUrl: '/api/auth' })
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 2. Wrap your app in the AblyProvider */}
+    <AblyProvider client={client}>
+      {/* 3. Wrap in a ChannelProvider if you want a global game channel */}
+      <ChannelProvider channelName="paintblitz-global">
+        <App />
+      </ChannelProvider>
+    </AblyProvider>
   </React.StrictMode>
 )
